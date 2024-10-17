f31c23d23723c935588b74bdfbd8d07755f91174
Arezou <Arezoughorbaniii@gmail.com>
2024-10-17 15:28:20 -0400

    ui in Main Activity updated

--- app/src/main/java/com/example/trackunit_assessment/MainActivity.kt
+++ app/src/main/java/com/example/trackunit_assessment/MainActivity.kt
@@ -4,14 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.trackunit_assessment.model.Order
 import com.example.trackunit_assessment.ui.theme.TrackUnit_AssessmentTheme
+import com.example.trackunit_assessment.viewModel.MainViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +42,87 @@ class MainActivity : ComponentActivity() {
         setContent {
             TrackUnit_AssessmentTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    OrdersScreen(innerPadding)
                 }
             }
         }
     }
+
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TrackUnit_AssessmentTheme {
-        Greeting("Android")
+        OrdersScreen(PaddingValues(0.dp))
     }
 }
+
+
+@Composable
+fun OrdersScreen(padding :PaddingValues,viewModel: MainViewModel = MainViewModel()) {
+    viewModel.getOrderData()
+    val uiState by viewModel.uiState.collectAsState()
+
+    when (uiState) {
+        is StateOrder.Idle -> {}
+        is StateOrder.Loading -> {
+            LoadingView()
+        }
+        is StateOrder.Data -> {
+            OrdersList((uiState as StateOrder.Data).data)
+        }
+    }
+}
+
+@Composable
+fun LoadingView() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Composable
+fun OrdersList(orders: List<Order>) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        items(orders.size) { index ->
+            OrderItem(orders[index])
+        }
+    }
+}
+
+
+@Composable
+fun OrderItem(order: Order) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        colors = CardDefaults.cardColors(containerColor = Color(0xFFF5F5F5)),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(
+                    imageVector = Icons.Default.ShoppingCart,
+                    contentDescription = null
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = "${order.items.size} Product in Order ${order.orderId}",
+                    style = MaterialTheme.typography.labelMedium
+                )
+            }}}}
